3d27b42d194b8be968aa2e1fc6cf58697f3e9e41
Lee Hyun Jun <mikey1000@naver.com>
2021-11-14 02:18:44 +0900

    url setting

--- post/urls.py
+++ post/urls.py
@@ -4,5 +4,9 @@ from post import views
 app_name = 'post'
 
 urlpatterns = [
-    path('', views.post_list, name="post_list")
+    path('', views.post_list, name="post_list"),
+    path('new/', views.post_new, name='post_new'),
+    path('edit/<int:pk>/', views.post_edit, name='post_edit'),
+    path('delete/<int:pk>/', views.post_delete, name='post_delete'),
+    
 ]
