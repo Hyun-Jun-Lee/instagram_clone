c079ee89bac3b25b77d2b783419c4f2f601846a8
Lee Hyun Jun <mikey1000@naver.com>
2021-11-14 22:05:59 +0900

    plus bookmark, like

--- post/urls.py
+++ post/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('new/', views.post_new, name='post_new'),
     path('edit/<int:pk>/', views.post_edit, name='post_edit'),
     path('delete/<int:pk>/', views.post_delete, name='post_delete'),
+    path('like/', views.post_like, name="post_like"),
+    path('bookmark/', views.post_bookmark, name="post_bookmark"),
     
 ]
