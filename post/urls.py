6e9c939a875de83b6dbbd21d91863ac59ae74b48
Lee Hyun Jun <mikey1000@naver.com>
2021-11-15 23:55:16 +0900

    comment url setting

--- post/urls.py
+++ post/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('delete/<int:pk>/', views.post_delete, name='post_delete'),
     path('like/', views.post_like, name="post_like"),
     path('bookmark/', views.post_bookmark, name="post_bookmark"),
+    path('comment/new/', views.comment_new, name="comment_new"),
+    path('comment/delete/', views.comment_delete, name="comment_delete"),
     
 ]
