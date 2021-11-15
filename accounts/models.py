ade0d519116808edced2070596a5c2e22dee6d21
Lee Hyun Jun <mikey1000@naver.com>
2021-11-16 00:44:48 +0900

    add following

--- accounts/models.py
+++ accounts/models.py
@@ -27,6 +27,11 @@ class Profile(models.Model):
                                 options={'quality': 90},
                                 blank=True,
                                 )
+    # follow field
+    follow_set = models.ManyToManyField('self',
+                                    blank=True, # follow 없어도 되도록 설정
+                                    through='Follow', # 중간 table = Follow
+                                    symmetrical=False,) # 비대칭 관계
     # 성별 선택창 / 왼쪽이 DB에 저장되는 이름, 오른쪽이 user 보이는 이름
     GENDER_C = (
         ('선택안함', '선택안함'),
@@ -41,3 +46,51 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.nickname
+    
+    # 해당 유저를 팔로워하고 있는 유저(follower) get
+    @property
+    def get_follower(self):
+        return [i.from_user for i in self.follower_user.all()]
+    
+    # 해당 유저가 팔로우하고 있는 중인 유저(following) get
+    @property
+    def get_following(self):
+        return [i.to_user for i in self.follow_user.all()]
+    
+    # 해당 유저를 팔로워하고 있는 유저 count
+    @property
+    def follower_count(self):
+        return len(self.get_follower)
+    
+    # 해당 유저가 팔로우하고 있는 중인 유저 count
+    @property
+    def following_count(self):
+        return len(self.get_following)
+    
+    # 어떤 유저가 해당 유저의 팔로워인지 팔로잉인지 확인하는 함수
+    def is_follower(self, user):
+        return user in self.get_follower
+    
+    def is_following(self, user):
+        return user in self.get_following
+    
+    
+    
+class Follow(models.Model):
+    # follow 받는 유저
+    from_user = models.ForeignKey(Profile,
+                                 related_name='follow_user',
+                                 on_delete=models.CASCADE)
+    # follow 하는 유저
+    to_user = models.ForeignKey(Profile,
+                               related_name='follower_user',
+                               on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return "{} -> {}".format(self.from_user, self.to_user)
+    
+    class Meta:
+        unique_together = (
+            ('from_user', 'to_user')
+        )
